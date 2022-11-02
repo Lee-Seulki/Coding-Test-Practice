0808ba3ccf52108c47f2f60a9e80fe543324ad59
Lee-Seulki <seulzzang1227@gmail.com>
2022-11-02 15:50:10 +0900

    modify code BOJ 1181

--- [BOJ]Step_by_step_solution/정렬/1181.py
+++ [BOJ]Step_by_step_solution/정렬/1181.py
@@ -5,7 +5,10 @@ input = sys.stdin.readline
 N = int(input())
 words = []
 for _ in range(N):
-    words.append(input().rstrip())
+    words.append(input().strip())    
 words = list(set(words))
+
 words.sort()
-print(words)
+words.sort(key = len)
+for word in words:
+    print(word)
