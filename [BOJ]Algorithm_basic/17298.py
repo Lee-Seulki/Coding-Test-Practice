3f64568e1999bc9635b38371ee91a10a6b5249b8
Lee-Seulki <seulzzang1227@gmail.com>
2022-11-13 17:50:58 +0900

    BOJ 17298

---
+++ [BOJ]Algorithm_basic/17298.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+lst = list(map(int, input().split()))
+answer = [-1] * len(lst)
+stack = []
+
+stack.append(0)
+for i in range(1, len(lst)):
+    while stack and lst[stack[-1]] < lst[i]:
+        answer[stack.pop()] = lst[i]
+    stack.append(i)
+
+print(*answer)
